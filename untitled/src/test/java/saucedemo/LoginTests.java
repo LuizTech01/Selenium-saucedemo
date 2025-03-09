f29f689d56fa06053e5a536211d2ca3c90eb2035
luizTech01 <luizflavio010@gmail.com>
2025-03-09 14:56:49 -0300

    adicionando mais cenarios de teste para o login e fazendo mais algumas validacoes

--- untitled/src/test/java/saucedemo/LoginTests.java
+++ untitled/src/test/java/saucedemo/LoginTests.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -68,9 +70,18 @@ public class LoginTests {
         navegador.findElement(By.id("password")).sendKeys("secret_sauce");
         navegador.findElement(By.id("login-button")).click();
 
-        String currentUrl = navegador.getCurrentUrl();
-        String expectedUrl = "https://www.saucedemo.com/v1/inventory.html";
-        assertEquals(expectedUrl, currentUrl, "A URL não foi redirecionada corretamente!");
+        WebDriverWait wait = new WebDriverWait(navegador, Duration.ofSeconds(10));
+        WebElement imgElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("inventory_item_img")));
+
+        // Obtém o src da imagem
+        String imgSrc = imgElement.getAttribute("src");
+        System.out.println("🔎 Verificando imagem com src: " + imgSrc);
+
+        if (isImageBroken(imgSrc)) {
+            System.out.println("❌ A imagem NÃO carregou corretamente (404 Not Found).");
+        } else {
+            System.out.println("✅ A imagem carregou corretamente.");
+        }
 
         navegador.quit();
     }
@@ -85,9 +96,72 @@ public class LoginTests {
 
         navegador.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
         navegador.findElement(By.id("password")).sendKeys("secret_sauce");
+        long startTime = System.currentTimeMillis();
         navegador.findElement(By.id("login-button")).click();
+
+        WebDriverWait wait = new WebDriverWait(navegador, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("inventory_container")));
+
+        long endTime = System.currentTimeMillis();
+
+        long tempoTotal = endTime - startTime;
+        System.out.println("⏳ Tempo de login: " + tempoTotal + " ms");
+
+        if (tempoTotal > 3000) {
+            System.out.println("⚠️ ALERTA: O login demorou mais do que o esperado!");
+        } else {
+            System.out.println("✅ O login foi realizado dentro do tempo aceitável.");
+        }
 
         navegador.quit();
     }
 
+    @Test
+    @DisplayName("Tentando logar sem informar usuario e senha")
+    public void testTentandoLogarSemInformarUsuarioESenha() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver navegador = new ChromeDriver();
+
+        navegador.get("https://www.saucedemo.com/v1/index.html");
+
+        navegador.findElement(By.id("login-button")).click();
+
+        WebElement errorMessage = navegador.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3"));
+        String actualText = errorMessage.getText();
+        assertTrue(actualText.contains("Epic sadface: Username is required"));
+
+        navegador.quit();
+    }
+
+    @Test
+    @DisplayName("Tentando logar sem senha")
+    public void testTentandoLogarSemInformarSenha() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver navegador = new ChromeDriver();
+
+        navegador.get("https://www.saucedemo.com/v1/index.html");
+
+        navegador.findElement(By.id("user-name")).sendKeys("standard_user");
+        navegador.findElement(By.id("login-button")).click();
+
+        WebElement errorMessage = navegador.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3"));
+        String actualText = errorMessage.getText();
+        assertTrue(actualText.contains("Epic sadface: Password is required"));
+
+        navegador.quit();
+    }
+
+    public static boolean isImageBroken(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            return responseCode != 200;
+        } catch (Exception e) {
+            return true;
+        }
+    }
 }
+
